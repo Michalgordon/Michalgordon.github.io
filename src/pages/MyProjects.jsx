5b5c14edbdb7632209aaf81bb437d082ed2136af
Michal Gordon <michalgordon92@gmail.com>
2024-01-24 10:18:47 -0800

    added pagination to projects

--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,8 +1,45 @@
 import { PROJECTS } from "../constants";
 import { ProjectCard } from "../components";
 import { motion } from "framer-motion";
-
+import React, {useEffect, useState, useRef} from "react";
 const MyProjects = () => {
+  
+  const projectsElement = useRef(null);
+
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+  const observerOptions = {
+    rootMargin: '0px',
+    threshold: 1
+}
+  useEffect(()=>{
+    let projectsClass = '.project';
+    
+     const observer = new IntersectionObserver(
+      (projects) => {
+        for (let project of projects) {
+          if (project.isIntersecting) {
+            setCurrentProjectIndex(project.target.id.replace(/\D/g, ""))
+          }
+        }
+      },
+      observerOptions
+    );
+    document.querySelectorAll(projectsClass).forEach((project) => {
+      if (project) {
+          observer.observe(project);
+      }
+  });
+
+    // cleanup function
+    return () => {
+      document.querySelectorAll(projectsClass).forEach((project) => {
+        if (project) {
+            observer.unobserve(project);
+        }
+    });
+    };
+  
+  });
 
   return (
     <div className="flex flex-col justify-center flex-grow laptop:px-4 py-10 laptop:px-20">
@@ -15,22 +52,23 @@ const MyProjects = () => {
         My Projects.
       </motion.h2>
       <motion.div
-        className="flex scroll-smooth self-center w-full px-10 flex-row snap-x justify-start laptop:justify-center overflow-auto gap-6 laptop:px-8"
+        className="flex scroll-smooth self-center w-full px-10 flex-row snap-x snap-mandatory justify-start laptop:justify-center overflow-auto gap-6 laptop:px-8"
         initial={{ x: "-200%" }}
         animate={{ x: 0 }}
         transition={{ duration: 0.7, type: "spring" }}
+        ref={projectsElement}
       >
         
         {PROJECTS.map((project, index) => (
-          <div className="slide snap-center" id={`slide-${index}`}>
-          <ProjectCard key={index} {...project} ></ProjectCard>
+          <div key={index}  className="project snap-center snap-always" id={`project-${index}`}>
+          <ProjectCard {...project} ></ProjectCard>
           </div>
         ))}
   
       </motion.div>
       <nav className="flex gap-4 self-center mt-4 laptop:hidden">
-     {PROJECTS.map((key, index) => (
-          <a className="w-4 h-4 rounded-full border-2 border-pewter-blue active:bg-pewter-blue"  href={`#slide-${index}`}></a>
+        {[...PROJECTS.keys()].map((index) => (
+          <span  className={`w-4 h-4 rounded-full border-2 border-pewter-blue ${currentProjectIndex == index? 'bg-pewter-blue' : ''}`} key={index}></span>
         ))}
         </nav>
     </div>
