42fda0fd5c3cf8624e8d8620c4d3ba5bbfb2fa3c
Michalgordon <Michalgordon@users.noreply.github.com>
2024-01-24 18:33:23 +0000

    deploy: 08522992de8b3ee38758950137b1fbd64bdc7724

---
+++ assets/ErrorPage-RX1Pxrhw.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-mhagUftK.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
--- assets/ErrorPage-c_QCkksk.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-0N5poJBz.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
