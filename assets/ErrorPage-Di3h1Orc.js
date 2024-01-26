7b13e8a7173245054b031474fd2672027177c37d
Michalgordon <Michalgordon@users.noreply.github.com>
2024-01-26 22:00:15 +0000

    deploy: bc87ca73dca31b8f326cda64cc34432b80fb5cc3

---
+++ assets/ErrorPage-Di3h1Orc.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Fqn0O4HN.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
--- assets/ErrorPage-rW2ERSAU.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-UMNWN21e.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
