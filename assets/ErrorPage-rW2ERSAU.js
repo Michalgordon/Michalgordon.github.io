48077ed5c93064effb097dd06e6a01e75adc2d8f
Michalgordon <Michalgordon@users.noreply.github.com>
2024-01-26 17:35:31 +0000

    deploy: b30c52759ee1e80856af35118900e511690c87ed

---
+++ assets/ErrorPage-rW2ERSAU.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-UMNWN21e.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
--- assets/ErrorPage-vy0oEQAP.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ifpsh8_1.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
