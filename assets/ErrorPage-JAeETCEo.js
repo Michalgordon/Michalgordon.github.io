b3b5ecb10eb177ba6e8736d544dbbd56c10d3304
Michalgordon <Michalgordon@users.noreply.github.com>
2024-01-27 17:22:36 +0000

    deploy: 0b21829a66672622cc1b02a25a9baf8224b196a2

---
+++ assets/ErrorPage-JAeETCEo.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-QhJH5IFT.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
--- assets/ErrorPage-TkWNJVK0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-TNBDSiFS.js";const s=()=>r.jsx("div",{children:"oops"});export{s as default};
